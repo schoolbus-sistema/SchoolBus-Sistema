65828382009da1f49590d5d84fe6a22f59f1275c
Caique Menezes <caiquemenezes1@outlook.com>
2024-04-16 11:58:24 -0300

    implentação completa da classe veículo. by:Caique

--- SchoolBus-Sistema/src/Veiculo.java
+++ SchoolBus-Sistema/src/Veiculo.java
@@ -1,54 +1,100 @@
+
+
 public class Veiculo {
     private String placa;
     private String modelo;
     private int ano;
     private int capacidade;
-    private String tipo;
+    private int tipo;
     private Contrato contrato;
+   
 
-    public Veiculo(String placa, String modelo, int ano, int capacidade, String tipo, Contrato contrato) {
+    public Veiculo(String placa, String modelo, int ano, int capacidade, int tipo, Contrato contrato) {
         this.placa = placa;
         this.modelo = modelo;
         this.ano = ano;
         this.capacidade = capacidade;
         this.tipo = tipo;
-        this.contrato = contrato;
+        if (tipo == 1) {
+           this.contrato = contrato;
+        } else if (tipo != 0 && tipo != 1) {
+            throw new IllegalArgumentException("Tipo de veículo inválido: " + tipo);
+        }
     }
+
+
+    public Veiculo(String placa, String modelo, int ano, int capacidade, int tipo) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.capacidade = capacidade;
+        this.tipo = tipo;
+    }
+
     public String getPlaca() {
-        return placa;
+        return this.placa;
     }
     public void setPlaca(String placa) {
         this.placa = placa;
     }
     public String getModelo() {
-        return modelo;
+        return this.modelo;
     }
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
     public int getAno() {
-        return ano;
+        return this.ano;
     }
     public void setAno(int ano) {
         this.ano = ano;
     }
     public int getCapacidade() {
-        return capacidade;
+        return this.capacidade;
     }
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
-    public String getTipo() {
-        return tipo;
+    public int getTipo() {
+        return this.tipo;
     }
-    public void setTipo(String tipo) {
+    public void setTipo(int tipo) {
         this.tipo = tipo;
     }
     public Contrato getContrato() {
-        return contrato;
+        if (this.contrato != null) {
+            return this.contrato;
+        } else {
+            throw new IllegalArgumentException(" Não possui contrato");
+        }
+    }
+    public int numContrato(){
+        if(this. contrato != null){
+            return contrato.getNumContrato();
+        }
+        else{
+            throw new IllegalArgumentException(" Veiculo não possui número de  contrato");
+        }
+        
+
     }
     public void setContrato(Contrato contrato) {
         this.contrato = contrato;
     }
 
-}
+    public void tipoVeiculo(){
+        switch (this.tipo){
+            case 1:
+                System.out.print("Esse veículo é Alugado e possui o número de contrato: " + contrato.getNumContrato());
+                break;
+            case 0:
+                System.out.print("Esse veículo é próprio");
+                break;
+            default:
+                System.out.print("Tipo de veículo inválido");
+        }
+    }
+    
+        
+    }
+
