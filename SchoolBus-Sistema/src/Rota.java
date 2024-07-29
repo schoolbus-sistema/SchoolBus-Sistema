1bf76ef391d99a85ef5ec96b93369f14f3561226
Caique Menezes <115851755+caiquedevjs@users.noreply.github.com>
2024-07-29 22:17:33 +0000

    classe Rota

---
+++ SchoolBus-Sistema/src/Rota.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.arreyList;
+
+public class Rota {
+
+    ArreyList<PontoDeParada> pontodeParada = new ArrayList<PontoDeParada>();
+    private Integer id;
+
+    public Rota(ArreyList<PontoDeParada> pontodeParada, String id){
+
+    }
+}
