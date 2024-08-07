e9e57457a19cb8a3ea3aa3d14e35631f24c62703
Caique Menezes <115851755+caiquedevjs@users.noreply.github.com>
2024-08-07 12:00:04 +0000

    implementação da classe rota

--- SchoolBus-Sistema/src/Rota.java
+++ SchoolBus-Sistema/src/Rota.java
@@ -1,12 +1,41 @@
 import java.util.*;
-import java.util.arreyList;
+import java.util.ArrayList;
 
-public class Rota {
+class Rota {
+    private static int totalRotas = 0; // Contador estático
+    private int id;
+    private ArrayList<PontoDeParada> pontosDeParada;
 
-    ArreyList<PontoDeParada> pontodeParada = new ArrayList<PontoDeParada>();
-    private Integer id;
+    public Rota() {
+        this.id = ++totalRotas; // Atribuição automática do ID
+        this.pontosDeParada = new ArrayList<>();
+    }
 
-    public Rota(ArreyList<PontoDeParada> pontodeParada, String id){
+    public int getId() {
+        return id;
+    }
 
+    public ArrayList<PontoDeParada> getPontosDeParada() {
+        return pontosDeParada;
+    }
+
+    public void setPontosDeParada(ArrayList<PontoDeParada> pontosDeParada) {
+        this.pontosDeParada = pontosDeParada;
+    }
+
+    public void adicionarPontoDeParada(PontoDeParada ponto) {
+        pontosDeParada.add(ponto);
+    }
+
+    public int calcularDemandaTotal() {
+        int demandaTotal = 0;
+        for (PontoDeParada ponto : pontosDeParada) {
+            demandaTotal += ponto.calcularDemandaTotal();
+        }
+        return demandaTotal;
+    }
+
+    public static void exibirTotalRotas() {
+        System.out.println("Total de rotas: " + totalRotas);
     }
 }
